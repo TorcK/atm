6383d170503af0b3815e80a9d3ec5b9d112452e6
TorcK <you@example.com>
2016-12-22 11:38:45 +0200

    add google analitycs

--- application/views/inc/footer.php
+++ application/views/inc/footer.php
@@ -1,3 +1,13 @@
 <footer class="container">
      ©<?=date('Y',time());?> <a href="/">at-motors.com.ua</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<a href="#">контакты</a>
 </footer>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-89343380-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
